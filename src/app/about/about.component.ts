790a11723404190adb986fd6fb977a8324de6478
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-09-07 18:48:27 -0700

    update rxjs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -14,6 +14,73 @@ export class AboutComponent implements OnInit {
     ngOnInit() {
 
 
+      /**
+       * And in those situations we can resort to the use of a subject
+       * as subject is at the same time an observer and an observable
+       * Let's create a new subject. We are going to define here a
+       * variable called subject and we are going to assign it a new
+       * subject that we are going to instantiate.
+       */
+
+      const subject = new Subject();
+
+      /**
+       * We can directly emit values with it, but we can also combine
+       * it with other observables, although we could use a subject
+       * here as a public member variable, for example, of this component
+       * and share it directly with other components of the application.
+       * That is usually not a good idea.
+       * The subject is meant to be private to the part of the application
+       * that is emitting a given set of data, the same way that here
+       * in our HTTP observable, we wouldn't want other parts of the
+       * program to get access here to this observer.
+       * Only this part of the program can emit errors,
+       * complete the observable or emit the backend response.
+       * It would not be a very good idea to share this observer outside
+       * of this method.
+       */
+      const series$ = subject.asObservable();
+
+      series$.subscribe(console.log);
+
+      subject.next(1);
+      subject.next(2);
+      subject.next(3);
+      subject.complete();
+
+      /**
+       * We don't have any way of providing unsubscribe logic to our
+       * observable that gets derived here from this subject.
+       *
+       * And we also run the risk of sharing accidentally the subject
+       * with other parts of the application, which means that those
+       * other parts of the application could potentially take over the
+       * behavior of the observable by directly calling the next complete
+       * or error on the subject, which is not intended by this reason,
+       */
+
+      /**
+       * we should try to use subjects as little as possible.
+       * Instead, we should try to derive our observables directly
+       * from the source as much as possible, using methods such as,
+       * for example, from promise to derive an observable from a promise
+       * or simply the from method that allows us to derive an observable
+       * directly from a browser event, for example.
+       *  So these are the preferred ways for creating our own observable
+       * by using these arrogates utility methods.
+       *
+       * from(document, 'keyup')
+       */
+
+      /**
+       * However, if by some reason that is not practical or even possible,
+       * then using a subject is a great way of creating a custom observable
+       * notice that another very common use case for our subjects is a
+       * multicasting. In the case of multicasting, we want to take one value
+       * from one observable stream and remit that into multiple separate
+       * output streams. As we will see, the notion of subject is going
+       * to be essential for us to implement our own custom store solution.
+       */
     }
 
 
--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,7 +1,22 @@
 import {Observable} from 'rxjs';
 
 
-export function createHttpObservable(url:string) {
+export function createHttpObservable(url: string) {
+  /**
+   * So here in our HTP observable, we have here a very clear
+   * separation between the observable that is getting returned here
+   * by the Create Method and the Observer, which is this parameter
+   * here that allows us to either emit a new value using next or
+   * complete or error out the observable in this way of creating
+   * observables. There is a very clear separation between the observable
+   * and the observer, but there are many situations where this is not a
+   * very convenient way of creating an observable. And in those situations
+   * we can resort to the use of a subject as subject is at the same time
+   * an observer and an observable let's switch here to the about
+   * component and quickly create a new subject. We are going to define
+   * here a variable called subject and we are going to assign it a
+   * new subject that we are going to instantiate.
+   */
     return Observable.create(observer => {
 
         const controller = new AbortController();
@@ -36,3 +51,16 @@ export function createHttpObservable(url:string) {
     });
 }
 
+
+/**
+ *  if some of those methods are not convenient or if we run into
+ * a source of data that is not easily transformable into an observable,
+ * or if we are doing the multicasting of one value to multiple
+ * separate observable consumers, then we might want to look into
+ * the notion of subject. So here in our HTP observable,
+ * we have here a very clear separation between the observable that is
+ * getting returned here by the Create Method and the Observer,
+ * which is this parameter here that allows us to either emit a new
+ * value using next or complete or error out the observable in this
+ * way of creating observables.
+ */
